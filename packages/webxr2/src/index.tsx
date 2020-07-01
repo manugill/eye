fd81f26b051fbae8320e5b9e131559d0c42d71f5
Manu <manugill1995@gmail.com>
2020-07-02 06:05:15 +1000

    webxr 2 boilerplate

---
+++ packages/webxr2/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+// import './static/index.css';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+export default App;
