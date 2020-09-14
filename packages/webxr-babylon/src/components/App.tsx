33063bd8f85a0b8ad44ebef993c811b64d4034b2
GaganUnvi <38974523+GaganUnvi@users.noreply.github.com>
2020-09-14 13:13:29 +0530

    Primrose default text added

--- packages/webxr-babylon/src/components/App.tsx
+++ packages/webxr-babylon/src/components/App.tsx
@@ -1,11 +1,12 @@
-import React, { useState, useRef } from 'react';
-import { Engine, Scene, useBabylonScene } from 'react-babylonjs';
-import { Vector3 } from '@babylonjs/core';
-import Hotkeys from 'react-hot-keys';
+import React, { useState, useRef } from "react";
+import { Engine, Scene, useBabylonScene } from "react-babylonjs";
+import { Vector3 } from "@babylonjs/core";
+import Hotkeys from "react-hot-keys";
 
-import Terminal from './Terminal';
-import Editor from './Editor';
+import Terminal from "./Terminal";
+import Editor from "./Editor";
 
+const cameraPosition = new Vector3(0, 0, -10);
 const lightVectors = [
   Vector3.Up(),
   Vector3.Down(),
@@ -16,29 +17,41 @@ const lightVectors = [
 const keyboardInput = (input) => {
   var output = {
     1: function () {
-      return console.log('onKeyDown', input);
+      return console.log("onKeyDown", input);
     },
     2: function () {
-      return console.log('onKeyUp');
+      return console.log("onKeyUp");
     },
   };
   return output[input.type]();
 };
 
 const mouseInput = (input) => {
-  console.log('mouse', input);
+  console.log("mouse", input);
 };
 
 const App = () => {
   const [focus, setFocus] = useState(undefined);
   const [scene, setScene] = useState(undefined);
+  const [pointerMove, setPointerMove] = useState(undefined);
+  const [pointerDown, setPointerDown] = useState(undefined);
+  const [pointerUp, setPointerUp] = useState(undefined);
+
+  const resetPointer = () => {
+    setPointerDown(undefined);
+    setPointerUp(undefined);
+  };
 
   const focusProps = (name: string) => ({
     setFocus: () => focus !== name && setFocus(name),
     focus: focus === name,
   });
 
-  console.log('focus', focus);
+  function mousemovef() {
+    var pickResult = scene.pick(scene.pointerX, scene.pointerY);
+
+    console.log(pickResult);
+  }
 
   return (
     <Engine canvasId="main-canvas" antialias={true}>
@@ -46,13 +59,30 @@ const App = () => {
         onKeyboardObservable={(e) => {
           keyboardInput(e);
         }}
+        onPointerMove={(e, pickResult, pointerType) => {
+          //console.log(pickResult);
+          //setPointerMove(e);
+          //mousemovef();
+          //resetPointer();
+        }}
+        onPointerUp={(e, pickResult) => {
+          // console.log(pickResult);
+          //setPointerUp(pickResult);
+        }}
         onPointerDown={
-          !scene
-            ? undefined
-            : (e) => {
-                const { pickedPoint } = scene.pick(e.x, e.y);
-                if (!pickedPoint) setFocus(undefined);
-              }
+          (e, pickResult) => {
+            // console.log(pickResult);
+            // console.log(e);
+
+            setPointerDown(pickResult);
+          }
+          // !scene
+          //   ? undefined
+          //   : (e) => {
+          //       setPointerDown(e);
+          //       const { pickedPoint } = scene.pick(e.x, e.y);
+          //       if (!pickedPoint) setFocus(undefined);
+          //     }
         }
         // onMeshPicked={(...params) => {
         //   console.log('onMeshPicked', ...params);
@@ -61,9 +91,9 @@ const App = () => {
           setScene(scene);
         }}
       >
-        <flyCamera
+        <followCamera
           name="camera1"
-          position={new Vector3(0, 0, -10)}
+          position={cameraPosition}
           setTarget={[Vector3.Zero()]}
         />
 
@@ -76,15 +106,19 @@ const App = () => {
           />
         ))}
 
-        <Editor />
+        <Editor
+          pointerMove={pointerMove}
+          pointerDown={pointerDown}
+          pointerUp={pointerUp}
+        />
 
         {/* <Terminal
           position={new Vector3(2, -3, 5)}
-          {...focusProps('terminal-1')}
+          {...focusProps("terminal-1")}
         />
         <Terminal
           position={new Vector3(0, 2, 0)}
-          {...focusProps('terminal-2')}
+          {...focusProps("terminal-2")}
         /> */}
       </Scene>
     </Engine>
--- packages/webxr-babylon/src/components/Editor.tsx
+++ packages/webxr-babylon/src/components/Editor.tsx
@@ -4,34 +4,67 @@ import React, {
   useEffect,
   useRef,
   MutableRefObject,
-} from 'react';
-import { Engine, Scene, CreatedInstance } from 'react-babylonjs';
-import { Vector3, Color4, Texture, DynamicTexture } from '@babylonjs/core';
-import { Primrose } from 'primrose/primrose.js';
+} from "react";
+import { Engine, Scene, CreatedInstance } from "react-babylonjs";
+import {
+  Vector3,
+  Color4,
+  Texture,
+  DynamicTexture,
+  Plane,
+} from "@babylonjs/core";
+import { Primrose } from "primrose/primrose.js";
+
+import defaultText from "./defaultText";
 
 type TextureRef = CreatedInstance<Texture>;
 type DynamicTextureRef = CreatedInstance<DynamicTexture>;
+type PlaneRef = CreatedInstance<Plane>;
 
-const Editor = ({ width = 8, height = 8 }) => {
+const Editor = ({ width = 8, height = 8, ...options }) => {
   const textureRef = useRef<TextureRef>();
   const dynamicTextureRef = useRef<DynamicTextureRef>();
+  const planeRef = useRef<PlaneRef>();
 
   const editor = useMemo(() => {
     return new Primrose({
       width: width * 100,
       height: height * 100,
-      scaleFactor: 2,
+      scaleFactor: 1,
       fontSize: 16,
     });
   }, []);
 
+  var tex = defaultText;
+  editor.value = tex;
+
+  const pointerDown = options.pointerDown;
+  const pointerUp = options.pointerUp;
+  if (pointerDown !== undefined) {
+    console.log("pointerDown", pointerDown);
+    console.log(Primrose.has(pointerDown.pickedMesh));
+    console.log(Primrose.get(pointerDown.pickedMesh));
+    var curEditor = Primrose.get(pointerDown.pickedMesh);
+
+    if (curEditor !== null) {
+      // curEditor.mouse.readDownEventUV(pointerDown);
+    } else if (Primrose.focusedControl !== null) {
+      Primrose.focusedControl.blur();
+    }
+  }
+  if (pointerUp !== undefined) {
+    console.log("pointerUp", pointerUp);
+  }
+
   useEffect(() => {
     const canvas = editor.canvas as OffscreenCanvas;
-    const context = canvas.getContext('2d');
+    const context = canvas.getContext("2d");
     const texture = textureRef.current.hostInstance;
+    const plane = planeRef.current.hostInstance;
 
-    console.log('textures', textureRef.current, dynamicTextureRef.current);
-    console.log('context', context);
+    console.log("textures", textureRef.current, dynamicTextureRef.current);
+    console.log("context", context);
+    console.log("plane", plane);
 
     const updateTexture = async () => {
       const blob = await canvas.convertToBlob();
@@ -42,16 +75,18 @@ const Editor = ({ width = 8, height = 8 }) => {
     updateTexture();
 
     // @Gagan: Do all the primrose magic in here
+    Primrose.add(plane, editor);
   }, []);
 
   return (
     <>
       <plane
+        ref={planeRef}
         name="plane"
-        width={4}
-        height={4}
+        width={8}
+        height={8}
         rotation={new Vector3(0, 0, 0)}
-        position={new Vector3(4, -2.1, 0)}
+        position={new Vector3(4, 0, 0)}
       >
         <standardMaterial
           name="material"
@@ -62,7 +97,7 @@ const Editor = ({ width = 8, height = 8 }) => {
             ref={textureRef}
             hasAlpha={true}
             assignTo="diffuseTexture"
-            url={'IMAGE_HERE'}
+            url={"IMAGE_HERE"}
           />
         </standardMaterial>
 
